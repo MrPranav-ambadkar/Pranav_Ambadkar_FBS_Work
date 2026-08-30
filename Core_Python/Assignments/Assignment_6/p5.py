ea87dae90227411e80b6fc28a3141c25395c1a67
MrPranav-ambadkar <139323437+MrPranav-ambadkar@users.noreply.github.com>
2026-08-30 11:05:03 +0530

    Add files via upload

---
+++ Core_Python/Assignments/Assignment_6/p5.py
@@ -0,0 +1,14 @@
+for i in range(1,6):
+    k=1
+    for j in range(1,6-i):
+        print(' ', end=' ')
+
+    for j in range(1,i+1):
+        print('*', end=' ')
+        k+=1
+
+    for j in range(1,i):
+        print('*', end=' ')
+        k+=1
+
+    print()
